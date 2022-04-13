1d10a08101eb2a1a34ef99e3293844d2bee0fd2e
Avinash Dod <dodavinash13@gmail.com>
2022-04-13 18:07:17 +0530

    added redux store , implementation left

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {Provider} from 'react-redux'
+import { createStore } from 'redux'
+import {rootReducer} from './redux/reducers/index'
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter as Router} from 'react-router-dom'
+
+const store =createStore(rootReducer)
 ReactDOM.render(
   <React.StrictMode>
-    <Router><App /></Router>
+    <Router><Provider store={store}><App /></Provider></Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
